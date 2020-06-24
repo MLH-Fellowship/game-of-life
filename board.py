c8361e5b7afab8d60ce29b00faaf745939e95311
Kaela Polnitz <kaelanpolnitz@gmail.com>
2020-06-24 13:38:33 -0700

    make part of board

---
+++ board.py
@@ -0,0 +1,18 @@
+#define board containing cells
+class Board:
+    #get board initialized from .txt file 
+    def init_board(self):
+        with open('test.txt') as b:
+            print(line)
+        self.display_board()
+        
+
+
+    #board class dipslay method -- current state
+    def display_board(self):
+        
+    
+
+
+
+
