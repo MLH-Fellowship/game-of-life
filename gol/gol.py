13e891174e19679f00b427191142c2d52bb3ef2d
korusuke <karan.sheth@somaiya.edu>
2020-06-25 02:21:19 +0530

    Add GOL rules

--- gol/gol.py
+++ gol/gol.py
@@ -1,4 +1,4 @@
-import os
+import numpy as np
 
 
 class GameOfLife:
@@ -7,7 +7,29 @@ class GameOfLife:
         with open('input.txt') as f:
             board = f.readlines()
         for row in board:
-            self.board.append([c for c in row.strip()])
+            self.board.append([int(c) for c in row.strip()])
+        self.board = np.array(self.board)
+
+    def count_neighbours(self, i, j, board):
+        """Returns sum of 8 immediate neighbours"""
+        area = board[max(0, i - 1): min(len(board), i + 2),
+                     max(0, j - 1): min(len(board[0]), j + 2)]
+        return np.sum(area) - board[i, j]
+
+    def step(self):
+        board = self.board
+        nboard = np.zeros((len(board), len(board[0])), dtype=int)
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                neighbours = self.count_neighbours(row, col, board)
+                if board[row][col] == 1 and (neighbours == 2 or neighbours == 3):  # noqa
+                    nboard[row][col] = 1  # survivial
+
+                if board[row][col] == 0 and neighbours == 3:
+                    nboard[row][col] = 1  # birth
+        board = nboard
+        self.board = board
+        return board
 
     def display(self):
-        return '\n'.join([''.join(row) for row in self.board])
+        return '\n'.join([''.join([str(c) for c in row]) for row in self.board])  # noqa
--- gol/main.py
+++ gol/main.py
@@ -1,5 +1,17 @@
 import gol
-
+import time
 game = gol.GameOfLife()
 
+print('Initial Board:')
 print(game.display())
+
+while True:
+    try:
+        game.step()
+        print(game.display())
+        time.sleep(0.5)
+    except KeyboardInterrupt:
+        print()
+        print('Final Board')
+        print(game.display())
+        exit(0)
