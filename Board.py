b9746cb2224ace6115e82b5e342a15142134b113
Lincoln Roth <lincolnmroth@gmail.com>
2020-06-24 16:31:26 -0400

    add Board and test text file

---
+++ Board.py
@@ -0,0 +1,20 @@
+
+class Board (object):
+    
+        def __init__(self, file):
+            self.board = []
+            for i in f:
+                row = []
+                for j in list(i):
+                    if (j.isdigit()):
+                        row.append(int(j))
+                self.board.append(row)
+
+        def display(self):
+            for i in self.board:
+                print(*i)
+
+if __name__=="__main__":
+    f = open("test.txt", "r")
+    b = Board(f)
+    b.display()
