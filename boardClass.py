37a26d6c3e87387a159abafa6a53eacc76eecff2
stellaw1 <stella01wang@gmail.com>
2020-06-24 14:02:32 -0700

    implemented toroidal boundary step function

--- boardClass.py
+++ boardClass.py
@@ -1,42 +1,47 @@
 
 class Board:
     board = []
+    numRows = 0
+    numCols = 0
 
     # initializes board using a text file
-    def __init__(board.txt):
-        file = open txt file 
-        for i in range[0,7]
-            col = []
-            for j in range[0,7]
-                cols.append(file.nextCharacter)
-            this.board.append(col)
+    def __init__(self, filepath = None):
+        if filepath:
+            with open(filepath) as fp:
+                lines = fp.readlines()
+                for line in lines:
+                    numRows += 1 
+                    line = line.strip()
+                    col = []
+                    for c in line:
+                        col.append(c)
+                    numCols = col.len()
+                    self.board.append(col)
 
     # display board's current state
-    def display():
-        for row in this.board:
-            for col in this.board:
-                print(col)
+    def display(self):
+        for row in range(self.numRows):
+            for col in range(self.numCols):
+                print(self.board[row][col])
             print('\n')
 
-    def step():
+    def step(self):
         # iterate through this.board in row major order
-        for row in this.board:
-            for col in this.board:
-                state = this.board[row - 1][col - 1] + this.board[row - 1][col] + this.board[row - 1][col + 1]
-                 + this.board[row][col - 1] + this.board[row][col + 1] + this.board[row + 1][col - 1] 
-                 + this.board[row + 1][col] + this.board[row + 1][col + 1]
+        for row in range(self.numRows):
+            for col in range(self.numCols):
+                
+                # compute 8-neghbor sum using toroidal boundary conditions - x and y wrap around  
+                state = this.board[(row - 1) % numRows][(col - 1) % numCols] + this.board[(row - 1) % numRows][col] + this.board[(row - 1) % numRows][(col + 1) % numCols] + this.board[row][(col - 1) % numCols] + this.board[row][(col + 1) % numCols] + this.board[(row + 1) % numRows][(col - 1) % numCols] + this.board[(row + 1) % numRows][col] + this.board[(row + 1) % numRows][(col + 1) % numCols]
 
-                # TODO need to consider border/ edge cases
-
-                if col == True: # alive
+                if self.board[row][col] == 1: # alive
                     # check if it has either 2 or 3 live neighbours
-                    if (state == 2 | state == 3)
+                    if state == 2 | state == 3:
                         pass
                     else 
                         col = 0
                 else: # dead
                     # check if it has 3 live neighbours
-                    if (state == 3)
+                    if state == 3:
                         col = 1
 
     def main():
