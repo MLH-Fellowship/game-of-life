e65be41f0a015df8c37b2d5cf978de64affbd7ff
Kaela Polnitz <kaelanpolnitz@gmail.com>
2020-06-24 14:21:46 -0700

    add some more checking

--- board.py
+++ board.py
@@ -19,24 +19,37 @@ class Board:
 
     #define main loop, step function 
 
-    def see_neighbors(self):
-        neighbors = []
+    def see_neighbors(self, b_row, b_column):
+        neighbors = [] #neighbors we will return
         for row in range(len(self.board)):
             for column in range(len(self.board[0])):
-                is_neighbor = True
+                is_valid_neighbor = True
                 #check each diretions 
+                next_row = row + b_row
+                next_column = column + b_column
+                
+                if (next_row < 0) or (next_row >= len(self.board)):
+                    is_valid_neighbor = false
+                if (next_column < 0) or (next_row >= len(self.board[0])):
+                    is_valid_neighbor = false
+                if (next_row < 0) or (next_row >= len(self.board)):
+                    is_valid_neighbor = false
+                if (is_valid_neighbor):
+                    neighbors.append(self.board[next_row][next_column])
+
+                #check within parameters 
         return neighbors
     def update_board(self):
             for row in range(len(self.board)):
                 for column in range(len(self.board[row])):
-                #check the neighbors each time 
-                get_neighbor = self.see_neighbors
-                #collect living cells 
-                cells_alive = []
-                #look through array 
-                for neighbor in get_neighbor:
-                    if neighbor.lives() 
-                        cells_alive.append(neighbor)
+                    #check the neighbors each time 
+                    get_neighbor = self.see_neighbors(row,column)
+                    #collect living cells 
+                    cells_alive = []
+                    #look through array 
+                    for neighbor in get_neighbor:
+                        if neighbor.lives():
+                            cells_alive.append(neighbor)
 
 
 
