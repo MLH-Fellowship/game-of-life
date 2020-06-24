ef6024ef51fa3054facdd20a4f82bcf2d8f57265
Ruby Werman <rubywerman@berkeley.edu>
2020-06-24 15:35:35 -0500

    make initial board and display

---
+++ board.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Board:
+    
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.grid = np.zeros(rows*cols).reshape((rows,cols))
+
+    def display(self):
+    	for row in self.grid:
+		    for val in row:
+		        print(val, end =" ")  
+		    print()
+
+p = Board(10,10)
+p.display()      
+
+
